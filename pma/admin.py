770966970222861b789bbdedb9d82ad3f3cf79c4
Marco Foi <foimarco@gmail.com>
2015-08-30 17:43:05 +0200

    Model 'Sito' registered in admin.py for being managed through ModelAdmin editing template

--- pma/admin.py
+++ pma/admin.py
@@ -1,6 +1,8 @@
-#from django.contrib import admin
+from django.contrib import admin
 from django.contrib.gis import admin as geoadmin
-from models import Punto
+from models import Punto, Sito
 
+# Register your geo-models here.
 geoadmin.site.register(Punto, geoadmin.GeoModelAdmin)
 # Register your models here.
+admin.site.register(Sito, admin.ModelAdmin)
