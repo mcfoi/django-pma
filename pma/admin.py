d41f90721f09571de58e3d27d3dccff34f5fae1e
Marco Foi <foimarco@gmail.com>
2015-08-30 17:34:39 +0200

    Model 'Punto' registered in admin.py for being managed through GeoModelAdmin editing template

--- pma/admin.py
+++ pma/admin.py
@@ -1,3 +1,6 @@
-from django.contrib import admin
+#from django.contrib import admin
+from django.contrib.gis import admin as geoadmin
+from models import Punto
 
+geoadmin.site.register(Punto, geoadmin.GeoModelAdmin)
 # Register your models here.
